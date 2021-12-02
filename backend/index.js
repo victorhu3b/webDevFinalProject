de0927e3e3be210520f1e5fad7c854a016f98b63
Hooninator <jbellavita@berkeley.edu>
2021-12-02 07:47:08 -0800

    working login, signup, and see_user routes. Signing up creates and returns a token, logging in returns the user's token. Tested with postman.

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const InitiateMongoServer = require("./config/db");
+const user = require("./routes/user"); 
+const auth = require("./middleware/auth");
+
+// Initiate Mongo Server
+InitiateMongoServer();
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
+
+
+app.use("/user", user);
+//app.use("/shop", shop);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
